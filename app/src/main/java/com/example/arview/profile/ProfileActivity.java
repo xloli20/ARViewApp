63099a1ad17300d23be5fc603893feb750fddb43
Alaa Atia Alzahrani <alaa.alzahrani88@gmail.com>
2019-03-10 21:05:28 +0300

    follow by username:fix bug

--- app/src/main/java/com/example/arview/profile/ProfileActivity.java
+++ app/src/main/java/com/example/arview/profile/ProfileActivity.java
@@ -181,7 +181,7 @@ public class ProfileActivity extends AppCompatActivity implements PostDetailsFra
           UserName.setText(p.getUserName());
           Name.setText(p.getName());
           profileDescription.setText(p.getProfileDescription());
-          NFollowers.setText(String.valueOf(p.getFollowers()));
+          NFollowers.setText(String.valueOf(p.getFollowers().size()));
           NPost.setText(String.valueOf(p.getPost()));
 
       }
